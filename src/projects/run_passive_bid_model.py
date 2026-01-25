048c5fc28d4ed1fa7cd544c66c3e5a31c0ba6d8d
Gavin Hancock <gjhancoc@gmail.com>
2026-01-25 12:30:14 -0800

    run script

--- src/projects/run_passive_bid_model.py
+++ src/projects/run_passive_bid_model.py
@@ -3,11 +3,11 @@ from passive_bid_model.config import Config
 from passive_bid_model.pipeline import run_pipeline
 
 cfg = Config(
-    raw_trades_dir=Path(r"D:\Quant_Research_Pipeline\data\00_raw\databento\equities\aapl\trades"),
-    raw_mbp1_dir=Path(r"D:\Quant_Research_Pipeline\data\00_raw\databento\equities\aapl\mbp1"),
-    norm_trades_dir=Path(r"D:\Quant_Research_Pipeline\data\01_normalized\equities\aapl\trades"),
-    norm_mbp1_dir=Path(r"D:\Quant_Research_Pipeline\data\01_normalized\equities\aapl\mbp1"),
-    grid_dir=Path(r"D:\Quant_Research_Pipeline\data\01_normalized\equities\aapl\1s_grid"),
+    raw_trades_dir=Path(r"Dir"),
+    raw_mbp1_dir=Path(r"Dir"),
+    norm_trades_dir=Path(r"Dir"),
+    norm_mbp1_dir=Path(r"Dir"),
+    grid_dir=Path(r"Dir"),
     bar_freq="1s",
     overwrite_norm=False,
     overwrite_grid=False,
